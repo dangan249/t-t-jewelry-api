2868b1761c65877224d747589b4ab6883fd0092a
An Dang <dang.an249@gmail.com>
2014-11-28 22:03:10 -0500

    tinh tien tu toi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TTJewelryApi::Application.config.secret_key_base = '2626082979362269fbe681ed80b82a5f331e0150ca0ffe5a76ecaeabbae2f70964aaa22933145ee2c89297253bdc7b492ddd5b2de077e42ebfc2d4e4543067e0'
